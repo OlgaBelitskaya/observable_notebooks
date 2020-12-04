c865d892501ac37171b9cd2eb105c0d035e4d3ee
Olga Belitskaya <safuolga@gmail.com>
2020-12-05 00:41:52 +0300

    update text-styling

--- text-styling/ac1eab1055f10af0@334.js
+++ text-styling/ac1eab1055f10af0@334.js
@@ -1,4 +1,4 @@
-// https://observablehq.com/@olgabelitskaya/text-styling@270
+// https://observablehq.com/@olgabelitskaya/text-styling@334
 export default function define(runtime, observer) {
   const main = runtime.module();
   main.variable(observer()).define(["md"], function(md){return(
@@ -7,30 +7,64 @@ md`# 📑 Text Styling`
   main.variable(observer()).define(["md"], function(md){return(
 md`## webkit-background-clip`
 )});
-  main.variable(observer()).define(["html","params","linear_gradient","string"], function(html,params,linear_gradient,string){return(
+  main.variable(observer("viewof deg")).define("viewof deg", ["html"], function(html){return(
+html`<input type='range' id='n_deg' value=120 min=0 max=360 style='width:200px'>`
+)});
+  main.variable(observer("deg")).define("deg", ["Generators", "viewof deg"], (G, _) => G.input(_));
+  main.variable(observer("viewof font_width")).define("viewof font_width", ["html"], function(html){return(
+html`<input type='range' id='n_font_width' value=200 min=100 max=900 style='width:200px'>`
+)});
+  main.variable(observer("font_width")).define("font_width", ["Generators", "viewof font_width"], (G, _) => G.input(_));
+  main.variable(observer("viewof font_size_px")).define("viewof font_size_px", ["html"], function(html){return(
+html`<input type='range' id='n_font_size_px' value=29 min=8 max=48 style='width:200px'>`
+)});
+  main.variable(observer("font_size_px")).define("font_size_px", ["Generators", "viewof font_size_px"], (G, _) => G.input(_));
+  main.variable(observer("viewof font_family")).define("viewof font_family", ["html"], function(html){return(
+html`<select id='n_font_family' style='width:200px'>
+  <option value='Akronim'>Akronim</option>
+  <option value='Miss Fajardose'>Miss Fajardose</option>
+  <option value='Smokum'>Smokum</option>
+  <option value='Tangerine'>Tangerine</option>
+  <option value='Wallpoet'>Wallpoet</option>
+  <option value='Satisfy'>Satisfy</option>
+  <option value='Aladin'>Aladin</option>
+</select>`
+)});
+  main.variable(observer("font_family")).define("font_family", ["Generators", "viewof font_family"], (G, _) => G.input(_));
+  main.variable(observer("viewof background_color")).define("viewof background_color", ["html"], function(html){return(
+html`<select id='n_background_color' style='width:200px'>
+  <option value='black'>black</option>
+  <option value='darkslategray'>darkslategray</option>
+  <option value='midnightblue'>midnightblue</option>
+  <option value='mintcream'>mintcream</option>
+  <option value='whitesmoke'>whitesmoke</option>
+  <option value='ghostwhite'>ghostwhite</option>
+  <option value='ivory'>ivory</option>
+  <option value='oldlace'>oldlace</option>
+</select>`
+)});
+  main.variable(observer("background_color")).define("background_color", ["Generators", "viewof background_color"], (G, _) => G.input(_));
+  main.variable(observer()).define(["html","font_family","background_color","params","deg","linear_gradient","font_width","font_size_px","string"], function(html,font_family,background_color,params,deg,linear_gradient,font_width,font_size_px,string){return(
 html`
 <style>
-@import 'https://fonts.googleapis.com/css?family=${params.font_family}';
+@import 'https://fonts.googleapis.com/css?family=${font_family}';
 </style>
-<div style='background:${params.background_color}; padding:${params.padding}vw;'>
-<div style='background:linear-gradient(${linear_gradient.deg}deg,
+<div style='background:${background_color}; padding:${params.padding}vw;'>
+<div style='background:linear-gradient(${deg}deg,
 ${linear_gradient.color1} ${linear_gradient.percent1}%,
 ${linear_gradient.color2} ${linear_gradient.percent2}%,
 ${linear_gradient.color3} ${linear_gradient.percent3}%,
 ${linear_gradient.color4} ${linear_gradient.percent4}%); 
-font:${params.font_width} ${params.font_size_px}px/${params.font_view_height} 
-var(--sans-serif); font-family:${params.font_family};
+font:${font_width} ${font_size_px}px/${params.font_view_height} 
+var(--sans-serif); font-family:${font_family};
 -webkit-background-clip:text; color:${params.font_color};'>
 ${string}</div></div>`
 )});
   main.variable(observer("params")).define("params", function(){return(
-{background_color:'darkslategray',padding:5,
-         font_family:'Akronim',font_width:200,
-         font_color:'transparent',
-         font_size_px:36,font_view_height:1.7}
+{padding:2,font_color:'transparent',font_view_height:1.1}
 )});
   main.variable(observer("linear_gradient")).define("linear_gradient", function(){return(
-{deg:120,color1:'magenta',percent1:0,
+{color1:'magenta',percent1:0,
                   color2:'orange',percent2:33,
                   color3:'cyan',percent3:67,
                   color4:'purple',percent4:100}
--- text-styling/index.js
+++ text-styling/index.js
@@ -1,1 +1,1 @@
-export {default} from "./ac1eab1055f10af0@270.js";
+export {default} from "./ac1eab1055f10af0@334.js";
